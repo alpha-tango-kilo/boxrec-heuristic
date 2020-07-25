dbb50852c92abe23f28bac6fa6fdf7ba5f5ecfa5
alpha-kilo-tango <atk@aaathats3as.com>
2020-07-25 11:04:55 +0100

    BoxRec searching

--- src/boxrec.rs
+++ src/boxrec.rs
@@ -2,8 +2,9 @@ use std::collections::HashMap;
 use std::error::Error;
 use std::io::{self, Write};
 
+use regex::Regex;
 use reqwest::blocking::{Client, Response};
-use scraper::Html;
+use scraper::{Html, Selector};
 use trim_in_place::TrimInPlace;
 
 use crate::Config;
@@ -96,10 +97,92 @@ fn am_beaned(response: &Response) -> bool {
 pub fn get_page_by_id(client: &Client, id: u32) -> Result<Html, Box<dyn Error>> {
     let url = format!("https://boxrec.com/en/proboxer/{}", id);
     let req = client.get(&url).send()?;
+
+    if am_beaned(&req) {
+        Err("Logged out by BoxRec".into())
+    } else {
+        let req_text = req.text()?;
+        //println!("{}", req_text);
+        Ok(Html::parse_document(req_text.as_str()))
+    }
+}
+
+pub fn boxer_search(client: &Client, forename: &str, surname: &str, active_only: bool) -> Result<u32, Box<dyn Error>> {
+    // Step 1: perform request
+    let forename = forename.to_lowercase();
+    let surname = surname.to_lowercase();
+    let url = format!(
+        "https://boxrec.com/en/search?p[first_name]={}&p[last_name]={}&p[role]=fighters&p[status]={}&pf_go=go&p[orderBy]=&p[orderDir]=ASC",
+        forename,
+        surname,
+        if active_only { "a" } else { "" }
+    );
+    let req = client.get(&url).send()?;
+
     if am_beaned(&req) {
         return Err("Logged out by BoxRec".into());
     }
-    let req_text = req.text()?;
-    //println!("{}", req_text);
-    Ok(Html::parse_document(req_text.as_str()))
+
+    // Step 2: parse results
+    let req = req.text()?;
+    let req = Html::parse_document(req.as_str());
+    let selector = Selector::parse("a.personLink").unwrap();
+    let mut results = req.select(&selector).peekable();
+    let re = Regex::new(r"[0-9]{3,}").unwrap();
+
+    let target;
+    let search_in;
+    let choices;
+
+    if results.peek().is_none() {
+        // Error if there are no results
+        return Err("No results".into());
+    } else if results.peek().unwrap().inner_html().to_lowercase() == format!("{} {}", forename, surname) {
+        // Exact match, accept
+        search_in = results.next()
+            .unwrap()
+            .html();
+        // Find ID of boxer using regex search
+        target = re.find(search_in.as_str()).unwrap().as_str();
+    } else {
+        // No exact match, list results and have user pick
+        println!("Exact match not found. Please choose your fighter");
+        choices = results.enumerate()
+            .map(|(n, er)| -> String {
+                println!("{}) {}", n+1, er.inner_html());
+                er.html()
+            })
+            .collect::<Vec<_>>();
+        // Handle user input
+        let choice: usize;
+        loop {
+            print!("Pick a number: ");
+            io::stdout().flush()?;
+            let mut temp = String::new();
+            io::stdin()
+                .read_line(&mut temp)?;
+            match temp.trim().parse::<usize>() {
+                Ok(n) => {
+                    if n > 0 && n < choices.len() {
+                        // Account for offset
+                        choice = n - 1;
+                        break;
+                    } else {
+                        println!("Please pick a valid number");
+                    }
+                },
+                Err(_) => println!("No, actually pick a number"),
+            }
+        }
+        // End user input
+        // Find ID of boxer using regex search
+        target = re.find(
+            choices.get(choice).unwrap()
+        ).unwrap()
+            .as_str();
+    }
+    // Parse String ID of boxer to u32
+    let boxer_id = target.parse::<u32>().unwrap();
+    println!("Selected: {}", boxer_id);
+    Ok(boxer_id)
 }
